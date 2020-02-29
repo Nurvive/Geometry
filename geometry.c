1f5e48de4eb9f0502139e0c4decfbd6bcb148139
Nurvive <jiimbo689@gmail.com>
2020-02-29 16:51:28 +0700

    refactoring names and arguments of functions

--- geometry.c
+++ geometry.c
@@ -1,10 +1,10 @@
 #include <math.h>
 #include <stdio.h>
-void area_of_circle(float);
-void perimeter_of_circle(float);
-void perimeter_and_area_of_triangle(float, float, float, float, float, float);
-void triangle_checker(float, float, float, float, float, float);
-void circle_checker(float, float, float);
+void area_of_circle(struct circle);
+void perimeter_of_circle(struct circle);
+void perimeter_and_area_of_triangle(struct triangle);
+void triangle_checker(struct triangle);
+void circle_checker(struct circle);
 
 struct point {
     float point1;
@@ -12,7 +12,7 @@ struct point {
 };
 struct circle {
     float radius;
-    struct point point_of_circle;
+    struct point center;
 };
 struct triangle {
     struct point first_point_of_triangle;
@@ -33,8 +33,8 @@ int main()
     Triangle.third_point_of_triangle.point2 = 0;
     Triangle.fourth_point_of_triangle.point1 = 0;
     Triangle.fourth_point_of_triangle.point2 = 0;
-    Circle.point_of_circle.point1 = 0;
-    Circle.point_of_circle.point2 = 0;
+    Circle.center.point1 = 0;
+    Circle.center.point2 = 0;
     Circle.radius = 0;
     char name[10];
     char first_letter;
@@ -43,15 +43,12 @@ int main()
     if (first_letter == 'c') {
         scanf("%5s%*c%f%f%*c%f",
               name,
-              &Circle.point_of_circle.point1,
-              &Circle.point_of_circle.point2,
+              &Circle.center.point1,
+              &Circle.center.point2,
               &Circle.radius);
-        circle_checker(
-                Circle.point_of_circle.point1,
-                Circle.point_of_circle.point2,
-                Circle.radius);
-        area_of_circle(Circle.radius);
-        perimeter_of_circle(Circle.radius);
+        circle_checker(Circle);
+        area_of_circle(Circle);
+        perimeter_of_circle(Circle);
     } else if (first_letter == 't') {
         scanf("%7s%*c%*c%f%f%*c%f%f%*c%f%f%*c%f%f%*c",
               name,
@@ -63,20 +60,8 @@ int main()
               &Triangle.third_point_of_triangle.point2,
               &Triangle.fourth_point_of_triangle.point1,
               &Triangle.fourth_point_of_triangle.point2);
-        triangle_checker(
-                Triangle.first_point_of_triangle.point1,
-                Triangle.first_point_of_triangle.point2,
-                Triangle.second_point_of_triangle.point1,
-                Triangle.second_point_of_triangle.point2,
-                Triangle.third_point_of_triangle.point1,
-                Triangle.third_point_of_triangle.point2);
-        perimeter_and_area_of_triangle(
-                Triangle.first_point_of_triangle.point1,
-                Triangle.first_point_of_triangle.point2,
-                Triangle.second_point_of_triangle.point1,
-                Triangle.second_point_of_triangle.point2,
-                Triangle.third_point_of_triangle.point1,
-                Triangle.third_point_of_triangle.point2);
+        triangle_checker(Triangle);
+        perimeter_and_area_of_triangle(Triangle);
     } else {
         printf("Error: unknown figure \n");
     };
