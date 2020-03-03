47dc761cf83f5e6ffcc7e1f6b4342c849b429654
Nurvive <jiimbo689@gmail.com>
2020-03-03 18:27:44 +0700

    finish refactoring function(float -> struct)

--- geometry.c
+++ geometry.c
@@ -1,40 +1,27 @@
+#include "geometry.h"
 #include <math.h>
 #include <stdio.h>
-void area_of_circle(struct circle);
-void perimeter_of_circle(struct circle);
-void perimeter_and_area_of_triangle(struct triangle);
-void triangle_checker(struct triangle);
-void circle_checker(struct circle);
 
-struct point {
-    float point1;
-    float point2;
-};
-struct circle {
-    float radius;
-    struct point center;
-};
-struct triangle {
-    struct point first_point_of_triangle;
-    struct point second_point_of_triangle;
-    struct point third_point_of_triangle;
-    struct point fourth_point_of_triangle;
-};
+void area_of_circle(circle Disk);
+void perimeter_of_circle(circle Disk);
+void perimeter_and_area_of_triangle(triangle Delta);
+void triangle_checker(triangle Delta);
+void circle_checker(circle Disk);
 
 int main()
 {
-    struct circle Circle;
-    struct triangle Triangle;
-    Triangle.first_point_of_triangle.point1 = 0;
-    Triangle.first_point_of_triangle.point2 = 0;
-    Triangle.second_point_of_triangle.point1 = 0;
-    Triangle.second_point_of_triangle.point2 = 0;
-    Triangle.third_point_of_triangle.point1 = 0;
-    Triangle.third_point_of_triangle.point2 = 0;
-    Triangle.fourth_point_of_triangle.point1 = 0;
-    Triangle.fourth_point_of_triangle.point2 = 0;
-    Circle.center.point1 = 0;
-    Circle.center.point2 = 0;
+    circle Circle;
+    triangle Triangle;
+    Triangle.first_point.x = 0;
+    Triangle.first_point.y = 0;
+    Triangle.second_point.x = 0;
+    Triangle.second_point.y = 0;
+    Triangle.third_point.x = 0;
+    Triangle.third_point.y = 0;
+    Triangle.fourth_point.x = 0;
+    Triangle.fourth_point.y = 0;
+    Circle.center.x = 0;
+    Circle.center.y = 0;
     Circle.radius = 0;
     char name[10];
     char first_letter;
@@ -43,8 +30,8 @@ int main()
     if (first_letter == 'c') {
         scanf("%5s%*c%f%f%*c%f",
               name,
-              &Circle.center.point1,
-              &Circle.center.point2,
+              &Circle.center.x,
+              &Circle.center.y,
               &Circle.radius);
         circle_checker(Circle);
         area_of_circle(Circle);
@@ -52,14 +39,14 @@ int main()
     } else if (first_letter == 't') {
         scanf("%7s%*c%*c%f%f%*c%f%f%*c%f%f%*c%f%f%*c",
               name,
-              &Triangle.first_point_of_triangle.point1,
-              &Triangle.first_point_of_triangle.point2,
-              &Triangle.second_point_of_triangle.point1,
-              &Triangle.second_point_of_triangle.point2,
-              &Triangle.third_point_of_triangle.point1,
-              &Triangle.third_point_of_triangle.point2,
-              &Triangle.fourth_point_of_triangle.point1,
-              &Triangle.fourth_point_of_triangle.point2);
+              &Triangle.first_point.x,
+              &Triangle.first_point.y,
+              &Triangle.second_point.x,
+              &Triangle.second_point.y,
+              &Triangle.third_point.x,
+              &Triangle.third_point.y,
+              &Triangle.fourth_point.x,
+              &Triangle.fourth_point.y);
         triangle_checker(Triangle);
         perimeter_and_area_of_triangle(Triangle);
     } else {
