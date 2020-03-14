db320d554ca2adad388ad1375578f4b537de1fe4
Nurvive <jiimbo689@gmail.com>
2020-03-14 17:08:08 +0700

    rename circle_checker -> radius_checker

---
+++ src/geometry.h
@@ -0,0 +1,22 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+struct point {
+  double x;
+  double y;
+};
+typedef struct circle {
+  double radius;
+  struct point center;
+} circle;
+typedef struct triangle {
+  struct point first_point;
+  struct point second_point;
+  struct point third_point;
+  struct point fourth_point;
+} triangle;
+void area_of_circle(circle Disk);
+void perimeter_of_circle(circle Disk);
+void perimeter_and_area_of_triangle(triangle Delta);
+void triangle_checker(triangle Delta);
+void radius_checker(circle Disk);
+#endif
